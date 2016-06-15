3796206ee6e2b19f046082da39df9a64a0d3d612
rocketNeck <homestarflyer@gmail.com>
2016-06-15 15:13:57 -0400

    add application controller pundit setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  include Pundit
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
     request.env['omniauth.origin'] || root_path
   end
+
+  private
+    def user_not_authorized
+      flash[:alert] = "Access Denied"
+      redirect_to (request.referrer || root_path)
+    end
 end

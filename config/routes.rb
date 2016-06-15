6790e21a79f6c0fef570694da75d5830be5260e0
rocketNeck <homestarflyer@gmail.com>
2016-06-15 15:05:47 -0400

    set up users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {:omniauth_callbacks => "users/omniauth_callbacks"}
-  resources :bikes
+  devise_for :users, :controllers => {:omniauth_callbacks => "users/omniauth_callbacks", :registrations_controller => "registrations_controller"}
   root 'bikes#index'
+  resources :bikes
+  resources :users
 
 
+#36:41  users controler
 
   # Example resource route with sub-resources:
   #   resources :products do

9894f2fd59f0847db8c4ddfcaf16f14ead48df30
Jesse L Lawson <homestarflyer@gmail.com>
2016-08-24 15:12:34 -0400

    created tag model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160803190031) do
+ActiveRecord::Schema.define(version: 20160824190550) do
 
   create_table "bikes", force: :cascade do |t|
     t.string   "company"
@@ -40,6 +40,14 @@ ActiveRecord::Schema.define(version: 20160803190031) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "tags", ["name"], name: "index_tags_on_name"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
